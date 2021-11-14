f549615f2b0e8769062389957d628326bbe61ba2
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2021-11-14 16:25:45 -0500

    Update dashboardStyles.js

--- src/components/Dashboard/styles/dashboardStyles.js
+++ src/components/Dashboard/styles/dashboardStyles.js
@@ -7,7 +7,6 @@ export const MainContainer = styled.div`
     align-items: center;
     background-color: hsl(220, 72%, 10%);
     height: 100vh;
-
     box-sizing: border-box;
 `
 export const SecondaryContainer = styled.div`
@@ -21,6 +20,17 @@ export const SecondaryContainer = styled.div`
                          "user stats stats stats";
     grid-gap: 1rem;
     padding: 20px;
+    
+
+    @media (max-width: 850px) {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        display: block;
+        width: 350px;
+        overflow: auto;
+        
+    }
 `
 export const UserPanel = styled.div`
     grid-area: user;
@@ -30,6 +40,10 @@ export const UserPanel = styled.div`
     display: flex;
     flex-direction: column;
     align-items: start;
+
+    @media (max-width: 850px) {
+        height: 150px;
+    }
 `
 export const UserPanelButtonContainer = styled.div`
     display: flex;
@@ -37,6 +51,12 @@ export const UserPanelButtonContainer = styled.div`
     justify-content: center;
     align-items: flex-start;
     height: 40%;
+
+    @media(max-width: 850px) {
+        flex-direction: row;
+        width: 100%;
+        border-radius: 0 0 10px 10px;
+    }
 `
 export const UserPanelButton = styled.button`
     margin: 0.5rem 0.5rem 0.5rem 1rem;
@@ -54,8 +74,11 @@ export const UserSubpanel = styled.div`
     height: 60%;
     border-radius: 10px;
     text-align: left;
-
     width: 100%;
+
+    @media(max-width: 850px) {
+        display: flex;
+    }
 `
 
 export const UserSubpanelImage = styled.img`
@@ -70,6 +93,10 @@ export const UserSubpanelTitle = styled.h2`
     margin: 1rem;
     font-weight: 200;
     color: white;
+
+    @media(max-width: 1150px) {
+        font-size: 20px;
+    }
 `
 export const UserSubpanelSubtitle = styled.p`
     margin: 1rem;
@@ -84,6 +111,11 @@ export const StatsPanel = styled.div`
     background-color: ${props => props.background};
     border-radius: 10px;
     position: relative;
+
+    @media(max-width: 850px) {
+        height: 150px;
+        margin-top: 1rem;
+    }
 `
 export const StatsImage = styled.img`
     height: 50px;
@@ -111,14 +143,28 @@ export const StatsSubpanelHeaderImage = styled.img`
     width: auto;
 `
 export const StatsSubpanelInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    @media(max-width: 850px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
 `
 export const StatsSubpanelInfoTitle = styled.h2`
     margin: 0 1rem 0 1rem;
     font-weight: 300;
     color: white;
+
 `
 export const StatsSubpanelInfoSubtitle = styled.p`
     margin: 1rem 1rem 0 1rem;
     font-size: 14px;
-    color: hsl(238, 31%, 68%);;
+    color: hsl(238, 31%, 68%);
+
+    @media(max-width: 850px) {
+        margin: 0 1rem 0 0;
+    }
 `
